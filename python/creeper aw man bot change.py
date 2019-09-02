9e942734be3971c299531bcaf964010c8f05f4d8
Zeta Gabriels <48227064+zetagabriels@users.noreply.github.com>
2019-09-02 14:06:24 -0500

    Delete creeper aw man bot change.py

--- python/creeper aw man bot change.py
+++
@@ -1,9 +0,0 @@
-async def creeper(message):
-	mycache = cache.Cache(message.guild.id)
-	if 'creeper' not in mycache.get_enabled_autos():
-		return False
-	
-	creeper_match = re.search(r'\bcreeper\b', message.content.lower())
-	if creeper_match:
-		await message.channel.send('aw man')
-		return True
